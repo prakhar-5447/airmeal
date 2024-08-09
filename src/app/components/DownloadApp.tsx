a37790e4b2f060ce132e90162c97751b128bff48
prakhar-5447 <sahuprakhar022003@gmail.com>
2024-08-09 14:39:16 +0530

    change css for responsive design

--- src/app/components/DownloadApp.tsx
+++ src/app/components/DownloadApp.tsx
@@ -1,21 +1,18 @@
 "use client";
+import { useState } from 'react';
 import Image from 'next/image';
 import styles from './DownloadApp.module.css';
 import appMockup from '../../../public/assets/app.png';
-import DownloadModal from '../utils/DownloadModal';
-import { useRef, useState } from 'react';
+import googlePlayBadge from '../../../public/assets/google.png';
+import appStoreBadge from '../../../public/assets/apple.png';
 
 export default function DownloadApp() {
-  const [isModalOpen, setModalOpen] = useState(false);
-  const buttonRef = useRef(null);
+  const [showBadges, setShowBadges] = useState(false);
 
   const handleDownloadClick = () => {
-    setModalOpen(true);
+    setShowBadges(!showBadges);
   };
 
-  const closeModal = () => {
-    setModalOpen(false);
-  };
   return (
     <>
       <div className={styles.wave}></div>
@@ -28,9 +25,14 @@ export default function DownloadApp() {
           <p className={styles.description}>
             Experience the convenience of drone delivery right at your fingertips. Download our app now and enjoy fast, reliable food delivery services.
           </p>
-          <button className={styles.button} onClick={handleDownloadClick} ref={buttonRef}>Download</button>
+          <button className={styles.button}>
+            Download
+            <div className={styles.badges}>
+              <Image src={googlePlayBadge} alt="Google Play Store" className={styles.storeBadge} />
+              <Image src={appStoreBadge} alt="App Store" className={styles.storeBadge} />
+            </div>
+          </button>
         </div>
-        {isModalOpen && <DownloadModal closeModal={closeModal} buttonRef={buttonRef} />}
       </section>
     </>
   );
--- src/app/components/VideoSection.tsx
+++ src/app/components/VideoSection.tsx
@@ -8,15 +8,15 @@ export default function VideoSection() {
   return (
     <>
       <section className={styles.videoSection}>
-          <video
-            className={styles.video}
-            autoPlay
-            loop
-            muted
-            src="/assets/drone.mp4"
-          >
-            Your browser does not support the video tag.
-          </video>
+        <video
+          className={styles.video}
+          autoPlay
+          loop
+          muted
+          src="/assets/drone.mp4"
+        >
+          Your browser does not support the video tag.
+        </video>
         <div className={styles.infoContainer}>
           <h1>About <i>AirMeal</i></h1>
           <p>
@@ -28,16 +28,22 @@ export default function VideoSection() {
         <h1>More about AirMeal</h1>
         <div className={styles.blogContainer}>
           <div className={styles.blogCard}>
-            <Image src={blog1} alt="No Image" className={styles.blogImage} />
-            <h1>Revolutionizing Food Delivery with Drones. Discover how drones are transforming the food delivery landscape with unmatched speed and efficiency.</h1>
+            <div className={styles.blogImageFrame}>
+              <Image src={blog1} alt="No Image" className={styles.blogImage} />
+            </div>
+            <h1>Revolutionizing Food Delivery with Drones. Discover how drones are transforming the food delivery landscape with unmatched speed and efficiency. To make the service work in a dense city, Meituan doesn’t have the drones deliver directly to your doorstep. </h1>
           </div>
           <div className={styles.blogCard}>
-            <Image src={blog2} alt="No Image" className={styles.blogImage} />
-            <h1>How Drones are Changing the Food Industry. Explore the impact of drones on the food industry and what it means for the future of food delivery.</h1>
+            <div className={styles.blogImageFrame}>
+              <Image src={blog2} alt="No Image" className={styles.blogImage} />
+            </div>
+            <h1>How Drones are Changing the Food Industry. Explore the impact of drones on the food industry and what it means for the future of food delivery. The economic viability for using drones to deliver food of course is still unproven.</h1>
           </div>
           <div className={styles.blogCard}>
-            <Image src={blog3} alt="No Image" className={styles.blogImage} />
-            <h1>The Future of Food Delivery: Drones and Beyond. Learn about the innovative technologies and advancements that are shaping the future of food delivery.</h1>
+            <div className={styles.blogImageFrame}>
+              <Image src={blog3} alt="No Image" className={styles.blogImage} />
+            </div>
+            <h1>The Future of Food Delivery: Drones and Beyond. Learn about the innovative technologies and advancements that are shaping the future of food delivery. Today’s drone-deliveries cover a broad range of goods: vaccines, medical supplies, and blood transfusions.</h1>
           </div>
         </div>
       </section>
