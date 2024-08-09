c7ca88e27988bdc0d21d5c91c76fb1067bc624c1
prakhar-5447 <sahuprakhar022003@gmail.com>
2024-08-09 15:31:05 +0530

    add deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/airmeal",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
